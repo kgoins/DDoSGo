5544225e0d675c1848aa1bb937ec05177dd69b09
Nick Werner <nickw_2013@yahoo.com>
2016-11-29 14:06:16 -0500

    Code for the agent registry system

--- src/network/agentRegistry.go
+++ src/network/agentRegistry.go
@@ -1,61 +1,74 @@
 package network
 
-
-import fmt
-
 /*AGENT REGISTRY- handler side
-	Initialized with this.start function
-	Accepts an incoming "register" message and adds that agent to the registry
-	Maintains a list of all of the agents a handler is in charge of
-	Removes agents when it shuts down or requests to be removed
-	Note: Each host should only have one agent
+Initialized with this.start function
+Accepts an incoming "register" message and adds that agent to the registry
+Maintains a list of all of the agents a handler is in charge of
+Removes agents when it shuts down or requests to be removed
+Note: Each host should only have one agent
 */
 
 /*MODIFYING AGENTS CLASS
-	Send a "register" message to handler with critical information about self
-		Critical information: Every router between itself and interwebz
+Send a "register" message to handler with critical information about self
+	Critical information: Every router between itself and interwebz
 */
 
 /*NEW REGISTER MSG
-	Utilize interface to create a new "register" message for agent to send to handler
+Utilize interface to create a new "register" message for agent to send to handler
 */
 
 /*LOOK/FEEL OF MSG
-	Critical info about agent
-	Add or Remove command
+Critical info about agent
+Add or Remove command
 */
 
-struct AgentRegistry{
-	//Hash map of agents
-}
-
-struct Agent{
+/* EXPECTED DEF OF AGENT*/
+type AgentRecord struct {
 	//What makes up an agent record
-	//host name, ip addr, port
-	//ArrayList trace
+	agent_hostname   string
+	handler_hostname string
+	handler_port     int
+	traceroute       []string //An empty list, use append to add to it
 }
 
-func start(){
-	//Initialize a hash map for agents	
-	//Create a channel for other objects to communicate with it
+type AgentRegistry struct {
+	//Hash map of agents
+	//The registry is a hashmap with hostnames as the key which returns the Agent record
+	registry map[string]AgentRecord
+}
+
+func start() *AgentRegistry {
+	//Initialize a hash map for agents
 	//Return a pointer to an agent registry
 	//Is the constructor
+	reg := make(map[string]AgentRecord)
+
+	return &AgentRegistry{
+		registry: reg}
 }
 
-func removeAgent(){
+func (reg *AgentRegistry) removeAgent(hostname string) {
 	//Removes an agent from the list of registered agents
-	//Throws error if it does not exist
+
+	delete(reg.registry, hostname)
+	//if hosname does not exist, delete does nothing
 }
 
-func addAgent(){
+func (reg *AgentRegistry) addAgent(agent AgentRecord) {
 	//Adds and agent to the registry (hash map)
+	reg.registry[agent.handler_hostname] = agent
 }
 
-func returnTrace(){
+func (reg *AgentRegistry) returnTrace(hostname string) []string {
 	//Returns all routers between an agent and the internet
 	//Returns whatever the config file has in it for a particular agent
 	//Return arrayList of host names?
+
+	agent, exists := reg.registry[hostname]
+
+	if exists {
+		return agent.traceroute
+	} else {
+		return nil
+	}
 }
-
-
-
