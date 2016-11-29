bccb5768d773a758faf887cc3c6b38618933b08e
Nick Werner <nickw_2013@yahoo.com>
2016-11-29 16:04:23 -0500

    New msg type: register for use with the AgentRegistry

--- src/msgs/register.go
+++ src/msgs/register.go
@@ -1,1 +1,20 @@
 package msgs
+
+import "network"
+
+type RegisterMsg struct {
+	record network.AgentRecord
+}
+
+func newRegisterMsg(rec network.AgentRecord) RegisterMsg {
+	return RegisterMsg{
+		record: rec}
+}
+
+func (regMsg RegisterMsg) String() string {
+	return "type: register; payload: agent= " + regMsg.record.GetAgHostname()
+}
+
+func (regMsg RegisterMsg) GetType() string {
+	return "register"
+}
