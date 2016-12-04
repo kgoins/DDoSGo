03f99789a5f538e2f5f960f53dedec6d44366e81
Nick Werner <nickw_2013@yahoo.com>
2016-12-04 16:57:12 -0500

    workers directory

---
+++ src/workers/MsgWorker.go
@@ -0,0 +1,30 @@
+package workers
+
+import "net"
+import "fmt"
+import "io/ioutil"
+
+import "msgs"
+
+// import "cmds"
+
+// implements dispatcher.Dispatchable
+type MsgDispatchable struct {
+	conn net.Conn
+}
+
+func NewMsgDispatchable(conn net.Conn) MsgDispatchable {
+	return MsgDispatchable{conn: conn}
+}
+
+func (msgDisp MsgDispatchable) DispatchableExec() {
+	defer msgDisp.conn.Close()
+	fmt.Println("handling conn from: " + msgDisp.conn.RemoteAddr().String())
+
+	msgBytes, _ := ioutil.ReadAll(msgDisp.conn)
+	msg := msgs.BuildMsg(msgBytes)
+	fmt.Println("received msg: " + msg.String())
+
+	cmd := msg.BuildCmd()
+	cmd.ExecCmd()
+}
